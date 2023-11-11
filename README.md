f3acefc9f994e2b37c8c4c9af234f292d73190de
asadfgglie <ac121878@outlook.com>
2023-11-12 00:27:15 +0800

    add gpt4 img comment add more detail logging new arg parser

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 3. make sure you have start [AUTOMATIC1111/stable-diffusion-webui](https://github.com/AUTOMATIC1111/stable-diffusion-webui) and has start flag `--api`
 4. start server
 ```commandline
-python app.py
+python app.py [--logging-level {CRITICAL,FATAL,ERROR,WARN,WARNING,INFO,DEBUG,NOTSET,critical,fatal,error,warn,warning,info,debug,notset}] [--env] [-h]
 ```
 
 ## code
@@ -14,3 +14,6 @@ python app.py
 * `util.py`: 輔助函數
 * `test.py`: 用來測試用的code
 * `config.json`: 設定檔
+
+## api doc
+You can start the server and go to http://127.0.0.1:5000 to read the doc
--- app.py
+++ app.py
@@ -1,16 +1,46 @@
-import asyncio
-import base64
-import json
+import argparse
+import os.path
+
+parser = argparse.ArgumentParser(
+    prog=os.path.basename(__file__),
+    description='This is AI digital frame backend server.')
+tmp = ['CRITICAL', 'FATAL', 'ERROR', 'WARN', 'WARNING', 'INFO', 'DEBUG', 'NOTSET']
+tmp = tmp + [i.lower() for i in tmp]
+parser.add_argument('-l', '--logging-level',
+                    choices=tmp,
+                    default='INFO',
+                    help='set logging level')
+parser.add_argument('--env', action='store',
+                    help='what `.env` file should be load.',
+                    default=False)
+
 import logging
+logging.basicConfig(level=parser.parse_args().logging_level.upper(), format='%(asctime)s %(levelname)s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+logging.info('Importing...')
+
+if parser.parse_args().env:
+    logging.info('load env values...')
+    from dotenv import load_dotenv
+    if load_dotenv(parser.parse_args().env):
+        logging.info(f'load `{parser.parse_args().env}` success.')
+    else:
+        logging.warning('no .env file has load.')
+
+import time
 
 import requests
+
+import io
+import asyncio
+import base64
+from PIL import Image
+
 from flask import Flask, request, jsonify, render_template
 
 import util
+logging.info('Import done.')
 
 app = Flask(__name__)
-
-VOICE_PROMPT = './VOICE_PROMPT.wav'
 
 @app.route('/')
 def index():
@@ -38,6 +68,7 @@ def generate():
     """
     args = request.json
     img = args.get('img')
+    Image.open(io.BytesIO(base64.b64decode(img))).save(util.IMG_INPUT)
     voice_prompt: str = args.get('voice', None)
     image_prompt: str = args.get('image_prompt', 'blonde hair,  long hair, fox ears, curled hair,  fox girl, (fluffy tail), fat tail, hands on the ground,'
             'gold eyes, black irises, long eyelashes, thick eyelashes,  looking at viewer,  smile, '
@@ -45,27 +76,31 @@ def generate():
             'thick thighs, ')
     bgm_prompt: str = args.get('bgm_prompt', 'casual, light, relaxing, piano music, ')
     if voice_prompt is not None:
-        with open(VOICE_PROMPT, 'wb') as f:
+        with open(util.VOICE_PROMPT, 'wb') as f:
+            logging.info('save ' + util.VOICE_PROMPT)
             f.write(base64.b64decode(voice_prompt))
-        voice_prompt = util.whisper_model.transcribe(VOICE_PROMPT)["text"]
+        voice_prompt = util.whisper_model.transcribe(util.VOICE_PROMPT)["text"]
         logging.info('transcribe voice: ' + voice_prompt)
 
-    payload = json.dumps({
+    logging.info('interrogate image prompt...')
+    t1 = time.time()
+    interrogate_img_prompt: str = requests.post("http://127.0.0.1:7860/sdapi/v1/interrogate", json={
         "image": img,
         "model": "clip"
-    })
+    }).json()['caption']
+    logging.info('interrogate image prompt done. take {:.2f} sec.'.format(time.time() - t1))
 
-    logging.info('interrogate image prompt...')
-    interrogate_img_prompt: str = requests.request('POST', "http://127.0.0.1:7860/sdapi/v1/interrogate", data=payload, headers={'Content-Type': 'application/json'}).json()['caption']
-
+    gpt4_reply = {
+        "img_prompt":'', "bgm_prompt": ''
+    }
     # gpt4_reply = util.GPT4_pipline(img, voice_prompt)
-
-    # gpt4_reply = json.loads(gpt4_reply)
+    # logging.info('gpt4_reply: ' + str(gpt4_reply))
 
     output_set: tuple[set[asyncio.Task], set] = asyncio.run(asyncio.wait([
-        util.stable_diffusion_pipline(image_prompt + interrogate_img_prompt,  #gpt4_reply["interrogate_img_prompt"]
+        util.stable_diffusion_pipline(image_prompt + interrogate_img_prompt + gpt4_reply["img_prompt"],
                                       img),
-        util.music_gen_pipline(bgm_prompt + (voice_prompt if voice_prompt is not None else '')#gpt4_reply["bgm_prompt"]
+        util.music_gen_pipline(bgm_prompt + ('"' + voice_prompt + '", ' if voice_prompt is not None else '') + gpt4_reply["bgm_prompt"],
+                               util.VOICE_PROMPT
     )]))
     output_set: list[asyncio.Task] = list(output_set[0])
     bgm = None
@@ -76,10 +111,11 @@ def generate():
         elif i.get_coro().__name__ is util.stable_diffusion_pipline.__name__:
             img = i.result()
 
-    # pic_comment = util.GPT4_pipline(img)
+    pic_comment = util.GPT4_pipline(img)
+    logging.info('GPT4 comment: ' + pic_comment)
 
     return jsonify({
-        'img_comment': 'test comment', #pic_comment,
+        'img_comment': pic_comment,
         'img': img,
         'bgm': bgm
     })
